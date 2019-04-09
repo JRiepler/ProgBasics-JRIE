c8f9018477d4261c9c3f707cbcad5503ca60e5d6
Jonas Riepler <jonas.riepler@htl-salzburg.ac.at>
2019-04-09 08:30:40 +0200

    Final commit of the assignment

---
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("erste Tests mit Operatoren");
+
+        //Multiplikative Operatoren
+        Debug.Log("Multiplikative Operatoren: * " + (3*4));
+        Debug.Log("Multiplikative Operatoren: / " + (9/3));
+        Debug.Log("Multiplikative Operatoren: % " + (4%4));
+
+        //Additive Operatoren
+        Debug.Log("Additive Operatoren: + " + (3+4));
+        Debug.Log("Additive Operatoren: - " + (4-3));
+
+        //Relationale Operatoren
+        Debug.Log("Relationale Operatoren: < " + (3<4));
+        Debug.Log("Relationale Operatoren: <= " + (3<=4));
+        Debug.Log("Relationale Operatoren: > " + (4>3));
+        Debug.Log("Relationale Operatoren: >= " + (4>=3));
+
+        //Gleichheits Operatoren
+        Debug.Log("Gleichheits Operatoren: == " + (4==4));
+        Debug.Log("Gleichheits Operatoren: != " + (3!=4));
+
+        //Logische Operatoren
+        Debug.Log("Logische Operatoren: & " + (3&4));
+        Debug.Log("Logische Operatoren: | " + (3|4));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
