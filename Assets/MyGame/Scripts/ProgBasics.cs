8f0b4774b61218903a4fc046563c41458df9860f
Jonas Riepler <jonas.riepler@htl-salzburg.ac.at>
2019-04-24 13:10:33 +0200

    edited README.md and Progbascis

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -1,86 +1,144 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ProgBasics : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        Operators();
-        Scurr();
-        Burr();
-        AddNumbers(400, 20);
-        MultiplicateFourNumbers(2, 4, 8, 16);
-        Debug.Log(MyText("JRIE"));
-        string drama = MyText("JRIE");
-        Debug.Log("neu" + drama);
-    }
-
-     public void Operators()
+public class ProgBasics : MonoBehaviour
+{
+    public int test1;
+    public float test2;
+    public string test3;
+    public int divi1 = 24;
+    public int divi2 = 2;
+    // Use this for initialization
+    void Start()
     {
-        Debug.Log("erste Tests mit Operatoren");
-
-        //Multiplikative Operatoren
-        Debug.Log("Multiplikative Operatoren: * " + (3 * 4));
-        Debug.Log("Multiplikative Operatoren: / " + (9 / 3));
-        Debug.Log("Multiplikative Operatoren: % " + (4 % 4));
-
-        //Additive Operatoren
-        Debug.Log("Additive Operatoren: + " + (3 + 4));
-        Debug.Log("Additive Operatoren: - " + (4 - 3));
-
-        //Relationale Operatoren
-        Debug.Log("Relationale Operatoren: < " + (3 < 4));
-        Debug.Log("Relationale Operatoren: <= " + (3 <= 4));
-        Debug.Log("Relationale Operatoren: > " + (4 > 3));
-        Debug.Log("Relationale Operatoren: >= " + (4 >= 3));
-
-        //Gleichheits Operatoren
-        Debug.Log("Gleichheits Operatoren: == " + (4 == 4));
-        Debug.Log("Gleichheits Operatoren: != " + (3 != 4));
-
-        //Logische Operatoren
-        Debug.Log("Logische Operatoren: & " + (3 & 4));
-        Debug.Log("Logische Operatoren: | " + (3 | 4));
-    } 
-
-    public void Scurr()
-    {
-        Debug.Log("Scurr");
+        //Methode im Start aufrufen sonst wird er nicht ausgeführt
+        test1 = 1;
+        Debug.Log("test 1 in Start" + test1);
+        // Operators();
+        Test2();
+        Test1();
+        intMult();
+        //meinName();
+        division();
+        // add values for parameters
+        AddNumbers(3, 2);
+        MultNumbers(2, 3, 1, 2);
+        MyDatas("18", "Student", "183cm");
+        SubNumbers(321, 123);
+        meinName("Jonas", "Riepler");
+        // all methodes combined
+        Combine();
+        //return type
+        Debug.Log(MyText("jriepler"));
+        string drama = MyText("jriepler");
+        Debug.Log("neu " + drama);
+        Debug.Log(Wohnort("Bischofshofen"));
+        Debug.Log(yo("Lil", "Lano"));
     }
-
-    public void Burr()
+    //Methode deklaiert
+    public void XY()
     {
-        Debug.Log(100 + 87);
+        Debug.Log("Methode XY");
     }
-
+    public void Test1()
+    {
+        Debug.Log("Multiplizieren jey: * " + (888 * 666));
+    }
+    public void Test2()
+    {
+        Debug.Log("Random Test: + " + (60 + 30));
+    }
+    public void division()
+    {
+        Debug.Log(divi1 / divi2);
+    }
+    //Methode deklaiert und Variablen deklariert
     public void AddNumbers(int a, int b)
+
     {
         int c = a + b;
-        Debug.Log("add numbers" + c);
+        Debug.Log("add numbers: " + c);
+        test1 = 3;
+        Debug.Log("test 1 in AddNumbers" + test1);
     }
-
-    public void MultiplicateFourNumbers(int w, int x, int y, int z)
+    public void MultNumbers(int a, int b, int c, int d)
     {
-        int v = w * x * y * z;
-        Debug.Log("MultiplicateFourNumbers" + v);
+        int e = a * b * c * d;
+        Debug.Log("mult numbers: " + e);
     }
-
+    //Methodes combined
     public void Combine()
     {
-        MultiplicateFourNumbers(2, 4, 8, 16);
-        AddNumbers(400, 20);
+        MultNumbers(4, 2, 1, 3);
         Operators();
+        AddNumbers(100, 1000);
     }
-
+    //Methode mit String
     public string MyText(string myName)
     {
-        return "Drama" + "" + myName;
+        return "Drama " + " " + myName; //leerer String
     }
-
-
+    //return type
+    public string Wohnort(string home)
+    {
+        return "Wohnort " + " " + home;
+    }
+    public void intMult()
+    {
+        //deklaration
+        int Mult1;
+        //initialisierung
+        Mult1 = 6;
+        int Mult2 = 2;
+        Debug.Log("int multi " + (Mult1 * Mult2));
+    }
+    //return type
+    public string yo(string vorname, string nachname)
+    {
+        return "Hello " + vorname + " " + nachname;
+    }
+    //parameter
+    public void MyDatas(string age, string edu, string height)
+    {
+        Debug.Log("Age: " + age + ", Status: " + edu + ", Height: " + height);
+    }
+    //parameter
+    public void SubNumbers(int a, int b)
+    {
+        int c = a - b;
+        Debug.Log("substract numbers: " + c);
+    }
+    //parameter
+    public void meinName(string vorname, string nachname)
+    {
+        Debug.Log("Vorname: " + vorname + " Nachname: " + nachname);
+    }
+    //Methode deklariert
+    public void Operators()
+    {
+        // Multiplikative Operatoren
+        Debug.Log("Multiplikative Operatoren: * " + (3 * 4));
+        Debug.Log("Multiplikative Operatoren: / " + (12 / 4));
+        Debug.Log("Multiplikative Operatoren: % " + (3 % 4));
+        //Additive Operatoren
+        Debug.Log("Additive Operatoren: + " + (3 + 4));
+        Debug.Log("Additive Operatoren: - " + (8 - 4));
+        //Relationale Operatoren 
+        Debug.Log("Relationale Operatoren: < " + (3 < 4));
+        Debug.Log("Relationale Operatoren: <= " + (3 <= 4));
+        Debug.Log("Relationale Operatoren: >" + (3 > 4));
+        Debug.Log("Relationale Operatoren: >= " + (3 >= 4));
+        //Gleichheits Operatoren 
+        Debug.Log("Gleichheits Operatoren: ==" + (5 == 5));
+        Debug.Log("Gleichheits Operatoren: !=" + (5 != 5));
+        //Logische Operatoren 
+        Debug.Log("Logische Operatoren: &" + (3 & 5));
+        Debug.Log("Logische Operatoren: |" + (3 | 5));
+        Debug.Log("Logische Operatoren: ^" + (3 ^ 5));
+    }
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
 }
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 <img src="./Screenshots/WelcomeScene.png" width="250">
 </div>
+Title: ProgBasics
+Progress: 100%
+Development Platform: Mac OS X	Unity 2018.2.14.f1	Visual Studio version 7.7.2.21	
+Target Platform: Standalone
+Description: Experimente mit Operatoren und Methoden, Code Formatiert 
+Copyright by Jonas Riepler :)
